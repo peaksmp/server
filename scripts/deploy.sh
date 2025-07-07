ac121e58f2eedee496a74a741afbdb5610fe086b
Caden Kriese <13630061+cadenkriese@users.noreply.github.com>
2025-07-06 20:16:47 -0600

    Add in-game update notification

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,16 +1,33 @@
 #! /bin/bash
 
-if [ -d "$SERVER_ROOT" ]
-then
-  cd "$SERVER_ROOT"
-  docker compose down
-  git fetch --all
-  git reset --hard origin/main
-else
-  git clone "$REPOSITORY_URL" "$SERVER_ROOT"
-  cd "$SERVER_ROOT"
-fi
+cat <<EOF | docker exec --interactive minecraft sh
+rcon-cli "playsound minecraft:block.note_block.bell block @a 0 0 0 1 0.5 1"
+sleep 0.2
+rcon-cli "playsound minecraft:block.note_block.bell block @a 0 0 0 1 0.5 1"
+sleep 0.1
+rcon-cli "playsound minecraft:block.note_block.bell block @a 0 0 0 1 0.749154 1"
+sleep 0.2
+rcon-cli 'title @a times 10 100 10'
+rcon-cli 'title @a subtitle {"text":"Peak will reboot to update in 15 seconds.","color":"#BFFFFF"}'
+rcon-cli 'title @a title {"text":"Server rebooting"}'
+EOF
 
-docker compose pull
-docker image prune -f
-docker compose up -d
+sleep 15
+
+docker exec minecraft rcon-cli "playsound minecraft:block.note_block.bit block @a 0 0 0 1 0.749154 1"
+
+
+#if [ -d "$SERVER_ROOT" ]
+#then
+#  cd "$SERVER_ROOT"
+#  docker compose down
+#  git fetch --all
+#  git reset --hard origin/main
+#else
+#  git clone "$REPOSITORY_URL" "$SERVER_ROOT"
+#  cd "$SERVER_ROOT"
+#fi
+#
+#docker compose pull
+#docker image prune -f
+#docker compose up -d
