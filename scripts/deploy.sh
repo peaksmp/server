4efba97b73758d4554946159fd474d9cc74a1a1d
Caden Kriese <13630061+cadenkriese@users.noreply.github.com>
2025-07-06 20:30:10 -0600

    Enable the rest of the deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,11 +2,10 @@
 
 cat <<EOF | docker exec --interactive minecraft sh
 rcon-cli "playsound minecraft:block.note_block.bell block @a 0 0 0 1 0.5 1"
-sleep 0.2
+sleep 0.25
 rcon-cli "playsound minecraft:block.note_block.bell block @a 0 0 0 1 0.5 1"
-sleep 0.1
+sleep 0.125
 rcon-cli "playsound minecraft:block.note_block.bell block @a 0 0 0 1 0.749154 1"
-sleep 0.2
 rcon-cli 'title @a times 10 100 10'
 rcon-cli 'title @a subtitle {"text":"Peak will reboot to update in 15 seconds.","color":"#BFFFFF"}'
 rcon-cli 'title @a title {"text":"Server rebooting"}'
@@ -14,20 +13,17 @@ EOF
 
 sleep 15
 
-docker exec minecraft rcon-cli "playsound minecraft:block.note_block.bit block @a 0 0 0 1 0.749154 1"
+if [ -d "$SERVER_ROOT" ]
+then
+  cd "$SERVER_ROOT"
+  docker compose down
+  git fetch --all
+  git reset --hard origin/main
+else
+  git clone "$REPOSITORY_URL" "$SERVER_ROOT"
+  cd "$SERVER_ROOT"
+fi
 
-
-#if [ -d "$SERVER_ROOT" ]
-#then
-#  cd "$SERVER_ROOT"
-#  docker compose down
-#  git fetch --all
-#  git reset --hard origin/main
-#else
-#  git clone "$REPOSITORY_URL" "$SERVER_ROOT"
-#  cd "$SERVER_ROOT"
-#fi
-#
-#docker compose pull
-#docker image prune -f
-#docker compose up -d
+docker compose pull
+docker image prune -f
+docker compose up -d
