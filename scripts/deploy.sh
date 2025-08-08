3039f9e29e3d7d2a38319fe33871e9a65fb97e01
Caden Kriese <13630061+cadenkriese@users.noreply.github.com>
2025-08-07 23:32:19 -0600

    Restructure deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,20 +1,20 @@
 #! /bin/bash
 
-cat <<EOF | docker exec --interactive minecraft sh
-rcon-cli "playsound minecraft:block.note_block.bell block @a 0 0 0 1 0.5 1"
-sleep 0.25
-rcon-cli "playsound minecraft:block.note_block.bell block @a 0 0 0 1 0.5 1"
-sleep 0.125
-rcon-cli "playsound minecraft:block.note_block.bell block @a 0 0 0 1 0.749154 1"
-rcon-cli 'title @a times 10 100 10'
-rcon-cli 'title @a subtitle {"text":"Peak will reboot to update in 15 seconds.","color":"#BFFFFF"}'
-rcon-cli 'title @a title {"text":"Server rebooting"}'
-EOF
-
-sleep 15
-
 if [ -d "$SERVER_ROOT" ]
 then
+  cat <<EOF | docker exec --interactive minecraft sh
+  rcon-cli "playsound minecraft:block.note_block.bell block @a 0 0 0 1 0.5 1"
+  sleep 0.25
+  rcon-cli "playsound minecraft:block.note_block.bell block @a 0 0 0 1 0.5 1"
+  sleep 0.125
+  rcon-cli "playsound minecraft:block.note_block.bell block @a 0 0 0 1 0.749154 1"
+  rcon-cli 'title @a times 10 100 10'
+  rcon-cli 'title @a subtitle {"text":"Peak will reboot to update in 15 seconds.","color":"#BFFFFF"}'
+  rcon-cli 'title @a title {"text":"Server rebooting"}'
+  EOF
+  
+  sleep 15
+
   cd "$SERVER_ROOT"
   docker compose down
   git fetch --all
